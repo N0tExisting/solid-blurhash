782f77218a67e61511971ed2a5156e9ab0591461
NotExisting <61290107+N0tExisting@users.noreply.github.com>
2022-01-08 19:29:48 +0100

    feat: (Almost) Canvas-less

--- src/Img.tsx
+++ src/Img.tsx
@@ -1,39 +1,70 @@
-import { mergeProps, Switch, Match, createSignal } from 'solid-js';
-import BlurhashCanvas from './Canvas';
+// @see https://github.com/woltapp/react-blurhash/issues/3
+import {
+	type ComponentProps,
+	type JSX,
+	splitProps,
+	mergeProps,
+	createMemo,
+	createSignal,
+} from 'solid-js';
+import createBlurhash from './createBlurhash';
 
-//extends JSX.HTMLAttributes<HTMLImageElement | HTMLCanvasElement>
-export interface BlurhashImgProps {
-	src: string;
+export interface BlurImgProps extends Omit<ComponentProps<'img'>, 'onload'> {
 	hash: string;
 	resX: number;
 	resY: number;
 	punch?: number;
-	/**
-	 * CSS height,
-	 * @default 128
-	 */
-	height?: number | string | 'auto';
-	/**
-	 * CSS width
-	 * @default 128
-	 */
-	width?: number | string | 'auto';
-	alt?: string;
 }
 
-const Img = (props: BlurhashImgProps) => {
-	const p = mergeProps({ width: 128, height: 128 }, props);
+const BlurImg = (allProps: BlurImgProps) => {
+	const [blur, p, rest] = splitProps(
+		mergeProps({ loading: 'lazy' }, allProps),
+		['hash', 'resX', 'resY', 'punch'],
+		['style', 'onLoad'],
+	);
 	const [loaded, setLoaded] = createSignal(false);
+	const blurhash = createBlurhash(blur.hash, blur.resX, blur.resY, blur.punch);
+	const style = createMemo((): JSX.CSSProperties | string | undefined => {
+		const hash = blurhash();
+
+		if (loaded() || hash == null) {
+			return p.style;
+		} else {
+			const baseStyles: JSX.CSSProperties = {
+				'background-image': `url(${hash})`,
+				// HACK: Make this more like https://gist.github.com/ngbrown/d62eb518753378eb0a9bf02bb4723235
+				'background-size': `cover`,
+			};
+
+			if (typeof p.style === 'string') {
+				return `${p.style};background-image:url(${hash});background-size:cover;`;
+			} else if (typeof p.style == null) {
+				return baseStyles;
+			} else {
+				return {
+					...p.style,
+					...baseStyles,
+				};
+			}
+		}
+	});
 	return (
-		<Switch>
-			<Match when={!loaded()}>
-				<BlurhashCanvas {...p} />
-			</Match>
-			<Match when={loaded()}>
-				<img {...p} alt={p.alt} onLoad={() => setLoaded(true)} />
-			</Match>
-		</Switch>
+		// eslint-disable-next-line jsx-a11y/alt-text -- its the consumer's job to provide alt text
+		<img
+			{...rest}
+			style={style()}
+			onLoad={(e) => {
+				setLoaded(true);
+				if (p.onLoad) {
+					if (typeof p.onLoad === 'function') {
+						p.onLoad(e);
+					} else {
+						p.onLoad[0](p.onLoad[1], e);
+					}
+				}
+			}}
+		/>
 	);
 };
 
-export default Img;
+export default BlurImg;
